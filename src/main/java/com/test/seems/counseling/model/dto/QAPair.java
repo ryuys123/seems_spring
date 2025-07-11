62fd525dd337b3e12c050a11c5952fb45a882df8
ryuys123 <zxswert@gmail.com>
2025-07-11 10:04:19 +0900

    챗봇 ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ

---
+++ src/main/java/com/test/seems/counseling/model/dto/QAPair.java
@@ -0,0 +1,23 @@
+package com.test.seems.counseling.model.dto;
+
+public class QAPair {
+    private String question;
+    private String answer;
+
+    // Getters and Setters
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
