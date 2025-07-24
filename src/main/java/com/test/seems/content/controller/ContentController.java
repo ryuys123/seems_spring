92543ca6791473c8468c0d0e90a56ee57c62ce95
LINUS0510 <jjs2245510@naver.com>
2025-07-24 17:36:41 +0900

    jjs_250724_v2

--- src/main/java/com/test/seems/content/controller/ContentController.java
+++ src/main/java/com/test/seems/content/controller/ContentController.java
@@ -1,4 +1,24 @@
 package com.test.seems.content.controller;
 
+import com.test.seems.content.jpa.entity.ContentEntity;
+import com.test.seems.content.model.service.ContentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ContentController {
+    @Autowired
+    private ContentService contentService;
+
+    // 감정ID로 추천 유튜브 컨텐츠 리스트 반환
+    @GetMapping("/content-recommendations/{emotionId}")
+    public ResponseEntity<List<ContentEntity>> getRecommendedContents(@PathVariable Long emotionId) {
+        List<ContentEntity> contents = contentService.getRecommendedContentsByEmotionId(emotionId);
+        return ResponseEntity.ok(contents);
+    }
 }
